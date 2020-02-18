7453da395e0992e3ee0ba9e0987ff017142026ee
mau-85 <mau-85@mail.ru>
2020-02-18 23:05:56 +0500

    doit

--- task9.py
+++ task9.py
@@ -1,14 +1,19 @@
-import numpy as np
+
+def average (ls):
+    sum = 0
+    for l in ls:
+        l = int(l)
+        sum += l
+    return sum/len(ls)
+
 with open(r'C:\Users\RobotOne\Downloads\task9_1.txt','r', encoding="utf-8") as inf:
+    makrMath,makrPhis,makrRus = [],[],[]
     for line in inf:
         line = line.strip()
         words = line.split(';')
-        print(words)
         marks = words[1:]
-        sum = 0
-        for mark in marks:
-            mark = int(mark)
-            sum += mark
-
-        print(sum/len(marks))
-        # print(np.mean(marks))
+        makrMath += [marks[0]]
+        makrPhis += [marks[1]]
+        makrRus += [marks[2]]
+        print(average(marks))
+    print(average(makrMath),average(makrPhis),average(makrRus))
